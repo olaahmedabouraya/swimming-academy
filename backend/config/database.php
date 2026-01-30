fd35951d35b9bcfc6a3904caf17224ed76c81a1a
olaahmedabouraya <olaahmedabouraya1998@gmail.com>
2026-01-30 06:40:22 +0200

    Add default socket path for localhost MySQL connections

--- backend/config/database.php
+++ backend/config/database.php
@@ -13,7 +13,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('DB_SOCKET', env('DB_HOST') === 'localhost' ? '/tmp/mysql.sock' : ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
